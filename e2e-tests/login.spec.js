98ef6c15c6f6a7ec6060b6b8f37299490db08755
arecouz <richardcouzens96@gmail.com>
2024-09-13 15:54:22 +0100

    .

--- e2e-tests/login.spec.js
+++ e2e-tests/login.spec.js
@@ -1,6 +1,6 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
-test('user can login, and create a new blog', async ({ page }) => {
+test('user can login, and click add new blog', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await page.locator('input[name="username"]').click();
   await page.locator('input[name="username"]').fill('user');
@@ -9,14 +9,4 @@ test('user can login, and create a new blog', async ({ page }) => {
   await page.getByRole('button', { name: 'login' }).click();
   await page.getByRole('heading', { name: 'user' }).click();
   await page.getByRole('button', { name: 'Add blog' }).click();
-  await page.getByPlaceholder('title').click();
-  await page.getByPlaceholder('title').fill('new');
-  await page.getByPlaceholder('author').click();
-  await page.getByPlaceholder('author').fill('new');
-  await page.getByPlaceholder('url').click();
-  await page.getByPlaceholder('url').fill('new');
-  await page.getByRole('button', { name: 'post' }).click();
-  await expect(
-    page.getByRole('heading', { name: 'new view' }).getByRole('button')
-  ).toBeVisible();
 });
