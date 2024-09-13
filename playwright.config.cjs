d81dddfcee1ea716facd92315cb95ba1293fc944
arecouz <richardcouzens96@gmail.com>
2024-09-13 16:08:44 +0100

    debugging pipeline error

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -71,7 +71,7 @@ module.exports = defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run server:dev',
+    command: 'npm run server',
     url: 'http://localhost:3000/',
     //   reuseExistingServer: !process.env.CI,
   },
